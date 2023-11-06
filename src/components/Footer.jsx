7e0d7614ee1d35068efb7ad50a400d46843e4d94
fayaz seedat <fayazseedat09@gmail.com>
2023-11-06 15:52:40 +0000

    updates

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,12 @@
 import React from "react";
+import Logo from "./logo192.png";
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <footer className="foot">
+            <img src={Logo} alt=""/>
+            <span>Made with <b>React.js</b>.</span>
+        </footer>
     )
 }
 
