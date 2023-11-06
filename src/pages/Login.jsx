6e5b5c6b929e58960bb02569287b823094ebce32
fayaz seedat <fayazseedat09@gmail.com>
2023-11-06 14:18:47 +0000

    update to learning how to use react

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
     return (
-        <div>Login2</div>
+        <div className="auth">
+            <h1>Login</h1>
+            <form>
+                <input required type="text" placeholder="username"/>
+                <input required type="password" placeholder="password" />
+                <button>Login</button>
+                <p>error message</p>
+                <span>Not registered? <Link to="/register"> Register</Link> </span>
+            </form>
+        </div>
     )
 }
 
